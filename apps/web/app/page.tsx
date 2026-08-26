d5f169258bc27edc64ce7b48eae5ec576c5d3efc
Nicholas Ly <nichdly@gmail.com>
2026-08-26 04:40:33 -0500

    Added collapsible JSON viewer

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,16 +1,19 @@
 "use client";
 
 import { evaluate } from "@nicholasdly/subscript";
-import { ExternalLinkIcon } from "lucide-react";
+import { BracesIcon, ExternalLinkIcon } from "lucide-react";
 import Link from "next/link";
 import { useState } from "react";
 
-import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible";
+import { InputGroup, InputGroupAddon, InputGroupInput } from "@/components/ui/input-group";
 
 export default function Page() {
   const [query, setQuery] = useState("2 lbs in grams");
+  const [isOpen, setIsOpen] = useState(true);
 
-  const result = JSON.stringify(evaluate(query), null, 2);
+  const result = evaluate(query);
 
   return (
     <main className="mx-auto my-8 flex max-w-lg flex-col gap-16 p-5">
@@ -38,10 +41,32 @@ export default function Page() {
         <p className="text-muted-foreground mb-4">
           Enter a natural language query below and see how it's evaluated.
         </p>
-        <Input className="mb-4" value={query} onChange={(e) => setQuery(e.target.value)} />
-        <div className="bg-muted overflow-scroll rounded-lg px-3 py-2">
-          <pre className="text-muted-foreground font-mono text-sm whitespace-pre">{result}</pre>
-        </div>
+        <Collapsible open={isOpen} onOpenChange={setIsOpen}>
+          <div className="flex gap-2">
+            <InputGroup>
+              <InputGroupInput value={query} onChange={(e) => setQuery(e.target.value)} />
+              {result.ok && (
+                <InputGroupAddon className="font-normal" align="inline-end">
+                  {result.text}
+                </InputGroupAddon>
+              )}
+            </InputGroup>
+            <CollapsibleTrigger
+              render={
+                <Button variant="outline" size="icon">
+                  <BracesIcon />
+                </Button>
+              }
+            />
+          </div>
+          <CollapsibleContent className="mt-4">
+            <div className="bg-muted overflow-scroll rounded-lg px-3 py-2">
+              <pre className="text-muted-foreground font-mono text-sm whitespace-pre">
+                {JSON.stringify(result, null, 2)}
+              </pre>
+            </div>
+          </CollapsibleContent>
+        </Collapsible>
       </div>
     </main>
   );
