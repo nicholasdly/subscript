a8af14d1f71e073a62f1f6dc754828d532304db7
Nicholas Ly <nichdly@gmail.com>
2026-08-26 04:12:45 -0500

    Updated website information

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,28 +1,47 @@
 "use client";
 
 import { evaluate } from "@nicholasdly/subscript";
+import { ExternalLinkIcon } from "lucide-react";
+import Link from "next/link";
 import { useState } from "react";
 
 import { Input } from "@/components/ui/input";
 
-const json = (data: unknown) => JSON.stringify(data, null, 2);
-
-const DEFAULT_QUERY = "2 lbs in grams";
-
 export default function Page() {
-  const [query, setQuery] = useState(DEFAULT_QUERY);
+  const [query, setQuery] = useState("2 lbs in grams");
 
-  const result = json(evaluate(query));
+  const result = JSON.stringify(evaluate(query), null, 2);
 
   return (
-    <main className="mx-auto my-8 max-w-md p-5">
-      <h1 className="mb-1 text-xl font-medium tracking-tight">nicholasdly/subscript</h1>
-      <p className="text-muted-foreground mb-4">
-        A natural language parser for unit conversion, time zone conversion, and basic math.
-      </p>
-      <Input className="mb-4" value={query} onChange={(e) => setQuery(e.target.value)} />
-      <div className="bg-muted overflow-scroll rounded-lg px-3 py-2">
-        <p className="text-muted-foreground font-mono text-sm whitespace-pre">{result}</p>
+    <main className="mx-auto my-8 flex max-w-lg flex-col gap-16 p-5">
+      <div>
+        <h1 className="mb-1 text-xl font-medium tracking-tight">
+          <Link
+            className="inline-flex items-center gap-2 decoration-1 underline-offset-4 hover:underline"
+            href="https://github.com/nicholasdly/subscript"
+          >
+            @nicholasdly/subscript <ExternalLinkIcon className="size-4" />
+          </Link>
+        </h1>
+        <p className="text-muted-foreground mb-4">
+          A natural language parser and evaluator for unit conversion, time zone conversion, and
+          basic math — zero dependencies and tree-shakeable.
+        </p>
+        <div className="bg-muted overflow-scroll rounded-lg px-3 py-2">
+          <pre className="text-muted-foreground font-mono text-sm">
+            npm install @nicholasdly/subscript
+          </pre>
+        </div>
+      </div>
+      <div>
+        <h2 className="mb-1 text-lg font-medium">Demonstration</h2>
+        <p className="text-muted-foreground mb-4">
+          Enter a natural language query below and see how it's evaluated.
+        </p>
+        <Input className="mb-4" value={query} onChange={(e) => setQuery(e.target.value)} />
+        <div className="bg-muted overflow-scroll rounded-lg px-3 py-2">
+          <pre className="text-muted-foreground font-mono text-sm whitespace-pre">{result}</pre>
+        </div>
       </div>
     </main>
   );
